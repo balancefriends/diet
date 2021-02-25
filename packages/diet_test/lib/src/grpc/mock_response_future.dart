d5907fe831599ef15307b34e9893400ece936dbf
dannyna <danny@pusan.ac.kr>
2021-02-25 10:04:38 +0900

    improvement: add trailers to responseFuture

--- packages/diet_test/lib/src/grpc/mock_response_future.dart
+++ packages/diet_test/lib/src/grpc/mock_response_future.dart
@@ -5,15 +5,22 @@ import 'package:mockito/mockito.dart';
 
 class MockResponseFuture<T> extends Mock implements ResponseFuture<T> {
   final Future<T> future;
+  final Map<String, String> _trailer;
 
-  MockResponseFuture.value(T value) : future = Future.value(value);
+  MockResponseFuture.value(T value) : future = Future.value(value), _trailer = {'grpc-status': '0'};
 
-  MockResponseFuture.error(Object error) : future = Future.error(error);
+  MockResponseFuture.error(Object error) : future = Future.error(error), _trailer = {'grpc-status': '2'};
 
-  MockResponseFuture.future(this.future);
+  MockResponseFuture.future(this.future) :  _trailer = {'grpc-status': '0'};
 
   @override
-  Future<S> then<S>(FutureOr<S> Function(T value) onValue,
-          {Function onError}) =>
+  Future<Map<String, String>> get trailers async => _trailer;
+
+  @override
+  Future<S> then<S>(FutureOr<S> Function(T value) onValue, {Function onError}) =>
       future.then(onValue, onError: onError);
+
+  @override
+  Future<T> catchError(Function onError, {bool Function(Object error) test}) =>
+      future.catchError(onError, test: test);
 }
